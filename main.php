5ce185c7c6acf8a0e0b026b41d771be0bbd42e95
Raven073 <98506458+Raven073@users.noreply.github.com>
2022-07-13 13:00:40 -0600

    Update main.php

--- main.php
+++ main.php
@@ -3,7 +3,7 @@ class MyDB extends SQLite3
 {
     function __construct()
     {
-        $this->open('Anagrafica.db');
+        $this->open('sample.sqlite');
     }
 }
 
@@ -13,10 +13,7 @@ if(!$db)
     echo $db->lastErrorMsg();
 }
 
-$hId = $_GET['hId']; //we're getting the passed hId as a paramater in the url
+$hId = $_GET['hId']; 
 
-$query = $db->prepare("SELECT name FROM Anagrafica WHERE hospital_ID=:id");
-$query->bindValue(':id', $hId, SQLITE3_INTEGER);
-$results = $query->execute()->fetchArray();
-echo $results['name'];
+echo working
 ?>
