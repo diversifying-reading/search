e7f64d4572f7b94c0164d9e381ee1660a04db24f
Raven073 <98506458+Raven073@users.noreply.github.com>
2023-01-27 08:21:30 -0700

    Update universal_topnav.js

--- js/universal_topnav.js
+++ js/universal_topnav.js
@@ -38,6 +38,8 @@ function scroll_function(){
     document.getElementsByClassName("topnav_text_links")[i].style.paddingTop = topnav_paddingTop + "px";
     document.getElementsByClassName("topnav_text_links")[i].style.paddingBottom = topnav_paddingTop + "px";
   }
+  document.getElementsByClassName("currentTextLink")[0].style.paddingTop = topnav_paddingTop + "px";
+  document.getElementsByClassName("currentTextLink")[0].style.paddingBottom = topnav_paddingTop + "px";
 
   if(document.getElementsByClassName("topnav_text_links").length > 0){
     document.getElementById("topnav").style.height = document.getElementsByClassName("topnav_text_links")[0].offsetHeight + "px";
@@ -52,8 +54,16 @@ function scroll_function(){
 
 function resize_topnav(){
   let options = ["Home", "Catalog", "Resources", "Suggest a Book"];
+  options.splice(options.indexOf(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0"), 1));
+  options.unshift(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0").charAt(0).toUpperCase() + currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0").slice(1))
 
-  let optionsFormatted = "<option> Home </option><option> Catalog </option>";
+  let optionsFormatted = "";
+
+  for(var i=0; i<options.length; i++){
+    optionsFormatted += "<option> ";
+    optionsFormatted += options[i];
+    optionsFormatted += "</option>"
+  }
 
   if(window.innerWidth <= 687 && document.getElementsByClassName("topnav_text")[0].style.height == ""){
     document.getElementsByClassName("topnav_text")[0].innerHTML = '<select class="select" onchange="selectTopnavUpdate()">' + optionsFormatted + '</select>';
@@ -94,5 +104,35 @@ function resize_topnav(){
   document.getElementById("menu_button").style.paddingLeft = document.getElementById("topnav").offsetHeight/3 +"px";
   document.getElementById("menu_button").style.paddingRight = document.getElementById("topnav").offsetHeight*2/7 +"px";
   document.getElementById("menu_button").style.paddingTop = (document.getElementById("topnav").offsetHeight-parseInt(document.getElementById("menu_button").style.height))/2-2.5 +"px";
-
+  document.getElementById("menu_button").style.paddingBottom = (document.getElementById("topnav").offsetHeight-parseInt(document.getElementById("menu_button").style.height))/2-2.5 +"px";
 }
+
+function currentRepository(url_test){
+  if(url_test.split("/")[3] == "search"){
+    return String("search");
+  }
+  else if(url_test.split("/")[4] == "resources.html"){
+    return String("resources");
+  }
+  else if(url_test.split("/")[4] == "suggest"){
+    return String("suggest");
+  }
+  else if(url_test.split("/")[3] == "diversifying-reading-slcpl"){
+    return String("home");
+  }
+}
+
+let topnav_text_links_index;
+if(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0") == "home"){
+  topnav_text_links_index = 0;
+}
+else if(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0") == "suggest"){
+  topnav_text_links_index = 1;
+}
+else if(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0") == "resources"){
+  topnav_text_links_index = 2;
+}
+else if(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0") == "search"){
+  topnav_text_links_index = 3;
+}
+document.getElementsByClassName("topnav_text_links")[topnav_text_links_index].className = "currentTextLink";
